ab2c1808f5b877b7325069cef929315c86b6e502
nithindaniel1 <nithindaniel2018@gmail.com>
2023-12-17 13:01:47 +0530

    created header and balace components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
-
+import Balance from './components/Balance';
+import Header from './components/Header';
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <div className="container">
+        <Balance/>
+      </div>
     </div>
   );
 }
